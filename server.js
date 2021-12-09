4b9bb24fcb512153b629560bdd2129a1af697b5f
Ashley <ashleyviola@Ashleys-MacBook-Pro.local>
2021-12-09 14:48:25 -0500

    added id sort functionality

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ function filterByQuery(query, animalsArray){
             );
         });
     }
-
     if(query.diet){
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
     }
@@ -46,6 +45,11 @@ function filterByQuery(query, animalsArray){
     }
     return filteredResults;
 }
+
+function findById(id, animalsArray){
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 //res is short for response req is short for request
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -53,4 +57,13 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if(result){
+        res.json(result);
+    } else{
+        res.send(404);
+    }
 });
